a5a01d24eae24a71b29eccb8dacef42242911b6e
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-16 11:55:26 +0900

    #3. 기본 타입 - 문자열, 숫자, 배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,15 @@
+// JS 문자열 선언
+let str = 'hello';
+
+// TS 문자열 선언
+let str2: string = 'hello';
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+
+// TS 배열(간단히)
+let items: number[] = [1, 2, 3];
