2a7e5acdabae0e1bed4f35858a2473968b9e3633
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-16 14:21:01 +0900

    #5. 함수의 매개변수를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,19 @@ interface User {
   name: string;
 }
 
+// 변수에 인터페이스 활용
 let seho: User = {
   age: 20,
   name: '세호',
 };
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+  console.log(user);
+}
+
+const capt = {
+  name: '캡틴',
+  age: 100,
+};
+getUser(capt);
