ac9ac3a9836111195a0847f9eeb7a68ab732566b
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-16 13:18:14 +0900

    #4. 변수, 함수 타입

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,7 @@
-let todoItems: any;
+let todoItems: object[];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): object[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -11,41 +11,43 @@ function fetchTodoItems() {
 }
 
 // CRUD methods
-function fetchTodos() {
+function fetchTodos(): object[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo): void {
+function addTodo(todo: object): void {
   todoItems.push(todo);
 }
 
 // 특정 인덱스에서 할 일 하나를 지움
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(index: number, todo: object): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): object[] {
   return todoItems.filter(item => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+function addTwoTodoItems(): {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개가 추가되어야 합니다.
+  addTodo({ id: 4, title: '자바', done: false });
+  addTodo({ id: 5, title: '스크립트', done: false });
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
