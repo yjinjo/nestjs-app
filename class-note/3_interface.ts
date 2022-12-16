6fcd85a254d56f2388b4f66809e4e5992b468daa
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-16 14:26:11 +0900

    #5. 함수 구조를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -19,3 +19,13 @@ const capt = {
   age: 100,
 };
 getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+  (a: number, b: number): number;
+}
+
+let sum: SumFunction;
+sum = function (a: number, b: number): number {
+  return a + b;
+};
