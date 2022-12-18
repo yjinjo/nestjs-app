573ad35fea9706b3d62af5fdd44b646bb8cd5ccc
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-18 18:11:40 +0900

    #7. Union Type의 특징

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -17,3 +17,19 @@ function logMessage2(value: string | number) {
 }
 logMessage2('hello');
 logMessage(100);
+
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+  someone.name;
+  // someone.skill;
+  // someone.age;
+}
