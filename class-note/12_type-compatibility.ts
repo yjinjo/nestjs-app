ccac70e5c4998edb95730ac194888fed729a487d
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-19 15:13:01 +0900

    #15. 타입 호환 예제 - 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -9,8 +9,8 @@ interface Person {
 
 let developer: Developer;
 let person: Person;
-developer = person;
-person = developer;
+// developer = person;
+// person = developer;
 
 interface Developer2 {
   name: string;
@@ -21,4 +21,31 @@ class Person2 {
 }
 let developer2: Developer2;
 let person2: Person2;
-developer = new Person2();
+// developer = new Person2();
+
+// 함수
+let add = function (a: number) {
+  // ...
+};
+let sum = function (a: number, b: number) {
+  // ...
+};
+// add = sum;
+sum = add;
+
+// 제네릭
+interface Empty<T> {
+  // ...
+}
+let empty1: Empty<string>;
+let empty2: Empty<number>;
+empty1 = empty2;
+empty2 = empty1;
+
+interface NotEmpty<T> {
+  data: T;
+}
+let notempty1 = NotEmpty<string>;
+let notempty2 = NotEmpty<number>;
+notempty1 = notempty2;
+notempty2 = notempty1;
