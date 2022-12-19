cde09adca683141152f35496234eecd12597f1f2
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-19 14:05:05 +0900

    #12. 복잡한 구조에서의 타입 추론 방식

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -15,3 +15,19 @@ let shoppingItem: Dropdown<string> = {
   value: 'abc',
   title: 'hello',
 };
+
+// 타입 추론 기본 3
+interface Dropdown2<T> {
+  value: T;
+  title: string;
+}
+interface DetailedDropdown2<K> extends Dropdown2<K> {
+  description: string;
+  tag: K;
+}
+let detailedItem: DetailedDropdown2<number> = {
+  title: 'abc',
+  description: 'ab',
+  value: 'a',
+  tag: 'a',
+};
