79e180ac699ac732e86f4af9ace3b4b78516742a
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-23 09:14:29 +0900

    #20. express 서버 사용해보기

---
+++ nodejs/ch17-express/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000); // 서버에 속성을 심음; port에 3000번
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
+
+app.post('/', (req, res) => {
+  res.send('hello express');
+});
+
+app.set('/about', (req, res) => {
+  res.send('hello express');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
