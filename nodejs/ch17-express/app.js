22c08a5072425bfac2cf008406fd6732bd7c470f
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-23 09:30:32 +0900

    #20. express로 html 서빙

--- nodejs/ch17-express/app.js
+++ nodejs/ch17-express/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 3000); // 서버에 속성을 심음; port에 3000번
 app.get('/', (req, res) => {
-  res.send('hello express');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/', (req, res) => {
