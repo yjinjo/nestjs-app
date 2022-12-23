43b5a36bd486d5735fe3e1b27f3e457d4a274c48
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-23 11:42:13 +0900

    #20. morgan, bodyParser, cookieParser

--- nodejs/ch17-express/app.js
+++ nodejs/ch17-express/app.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 app.set('port', process.env.PORT || 3000); // 서버에 속성을 심음; port에 3000번
-app.use((req, res, next) => {
-  console.log('모든 요청에 실행하고싶어요');
-  next();
-});
+
+app.use(morgan('dev'));
+app.use(cookieParser('zerochopassword'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+  req.body;
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/category/:name', (req, res) => {
-  res.send(`hello ${req.params.name}`);
+app.post('/', (req, res) => {
+  res.send('hello express!');
 });
 
-app.set('/about', (req, res) => {
-  res.send('hello express');
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(200).send('에러!!!!');
 });
 
 app.listen(app.get('port'), () => {
