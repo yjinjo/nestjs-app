8126d2d9e65d89cacaf2a1fe1f38528b7f0c92d1
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-25 12:53:25 +0900

    #20. Pug 템플릿 엔진

--- nodejs/ch17-express/app.js
+++ nodejs/ch17-express/app.js
@@ -14,6 +14,8 @@ const userRouter = require('./routes/user');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
