4b27b3bf9490a9466dd29a26b10dd9981808d436
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-25 23:00:23 +0900

    #21. Sequelize 모델 생성

--- nodejs/ch18-mysql/models/index.js
+++ nodejs/ch18-mysql/models/index.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
 
 const env = process.env.NODE_ENV || 'development';
 
@@ -14,5 +16,14 @@ const sequelize = new Sequelize(
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.init(sequelize);
+Comment.init(sequelize);
+
+User.associate(db);
+Comment.associate(db);
 
 module.exports = db;
