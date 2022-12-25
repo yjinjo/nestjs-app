8b983745e1ba7efd7bff0c8d45fce43dce7074ec
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-25 12:10:22 +0900

    #20. 라우터 분리

--- nodejs/ch17-express/app.js
+++ nodejs/ch17-express/app.js
@@ -1,11 +1,16 @@
 const dotenv = require('dotenv');
-dotenv.config();
 
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const multer = require('multer');
+const fs = require('fs');
+
+dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
@@ -28,45 +33,13 @@ app.use(
   })
 );
 
-const multer = require('multer');
-const fs = require('fs');
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 
-try {
-  fs.readdirSync('uploads');
-} catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  fs.mkdirSync('uploads');
-}
-const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, 'uploads/');
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname);
-      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
-app.get('/upload', (req, res) => {
-  res.sendFile(path.join(__dirname, 'multipart.html'));
-});
-app.post('/upload', upload.single('image'), (req, res) => {
-  console.log(req.file);
-  res.send('ok');
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
 });
 
-app.get(
-  '/',
-  (req, res, next) => {
-    console.log('GET / 요청에서만 실행됩니다.');
-    next();
-  },
-  (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
-  }
-);
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send(err.message);
